002149f916c767d1ed0678741c42e8b0d230b124
16046512 <16046512@myrp.edu.sg>
2018-05-28 11:52:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16046512/demoalarmmanager/AlarmReveiverActivity.java
@@ -0,0 +1,13 @@
+package com.example.a16046512.demoalarmmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmReveiverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_reveiver);
+    }
+}
